ff08a4d1bf3dbf91f9a7a4ca0b491e08eadee2c8
emma munday <elm74@cam.ac.uk>
2022-02-07 20:59:51 +0000

    task 1b test

---
+++ test_stations_by_distance.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+from floodsystem.geo import stations_by_distance
+from floodsystem.stationdata import build_station_list
+
+def test_stations_by_river():
+    stations = build_station_list()
+    p = 52.2053, 0.1218
+    x = stations_by_distance(stations, p)
+
+    assert type(x) is list
+    
+    for i in range(len(x)):
+        assert type(x[i]) is tuple
+        assert x[i][1] >= 0
+
+test_stations_by_river()
