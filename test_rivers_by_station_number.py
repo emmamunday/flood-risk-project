29f80b73f2b900b81c73441c82a09fd28676fc01
emmamunday <elm74@cam.ac.uk>
2022-02-08 12:11:47 +0000

    fix to test

--- test_rivers_by_station_number.py
+++ test_rivers_by_station_number.py
@@ -1,12 +1,12 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_by_station_number
 
-def test_rivers_by_station_number(N):
+def test_rivers_by_station_number():
     stations = build_station_list()
-    x = rivers_by_station_number(stations, N)
+    x = rivers_by_station_number(stations, 7)
 
     assert type(x) is list
-    assert len(x) >= N
+    assert len(x) >= 7
     for i in range(len(x)):
         assert type(x[i]) is tuple
         assert x[i][1] >= 1
