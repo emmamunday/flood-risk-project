cf830391713d6d7a454d67c6a9d0dc955476e1d8
Vincent Yung <vnsy2@cam.ac.uk>
2022-02-20 23:26:42 +0000

    task 2b changes

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,10 +1,12 @@
 from .utils import sorted_by_key
 from floodsystem.station import MonitoringStation
-from floodsystem.stationdata import  update_water_levels
+from floodsystem.stationdata import  update_water_levels,build_station_list
 
 def stations_level_over_threshold(stations, tol):
     rivers = []
-    #stations= build_station_list()
+    stations= build_station_list()
+    update_water_levels(stations)
+    
     for station in stations:
 
         if  station.relative_water_level() > tol:
