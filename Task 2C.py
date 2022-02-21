0b878e3861f3df7bb937a1c5d1b668215e664fa3
Vincent Yung <vnsy2@cam.ac.uk>
2022-02-21 11:35:52 +0000

    task 2c

---
+++ Task 2C.py
@@ -0,0 +1,20 @@
+from floodsystem.stationdata import build_station_list,update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+
+def run():
+    """Requirements for Task 2C"""
+
+    # Build list of stations
+    stations = build_station_list()
+    update_water_levels(stations)
+    
+    rivers_sorted = stations_highest_rel_level(stations, 10)
+    for station in rivers_sorted:
+
+        print(station[0],station[1])
+
+
+
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -14,3 +14,14 @@ def stations_level_over_threshold(stations, tol):
 
     rivers = sorted_by_key(rivers,1,True)
     return rivers  
+
+def stations_highest_rel_level(stations, N):
+    rivers = []
+    for station in stations:
+        if station.typical_range_consistent() ==True:
+            if  station.relative_water_level()==None:
+                pass
+            else:
+                rivers.append((station.name,station.relative_water_level()))
+    rivers = sorted_by_key(rivers,1,True)[:N]
+    return rivers
