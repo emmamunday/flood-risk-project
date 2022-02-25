f7256487b10843e16353803d164772df42bf29f8
emmamunday <elm74@cam.ac.uk>
2022-02-25 20:13:38 +0000

    fix2

--- test_polyfit.py
+++ test_polyfit.py
@@ -1,16 +1,15 @@
 from floodsystem.analysis import polyfit
 import datetime
-import matplotlib as mpl
+import matplotlib.dates
 import numpy as np
 
 def test_polyfit():
-    import datetime
 
     shift = 1
     power = 2
     x = [0, 1, 2, 3, 4]
     y = [1, 4, 9, 16, 25]
-    num = mpl.dates.date2num(x)
+    num = matplotlib.dates.date2num(x)
     coeff = np.polyfit(num - num[shift - 1], y, power)
     poly1 = np.poly1d(coeff)
 
